322cfc672f8cc044016d5eef8ad2fcdd4cabd2ef
cliff <SimonSungm>
2020-05-12 13:53:17 +0800

    original

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 66 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#66 SMP PREEMPT Mon May 11 20:02:15 CST 2020"
+#define LINUX_COMPILE_BY "cliff"
+#define LINUX_COMPILE_HOST "cliff-lab"
+#define LINUX_COMPILER "gcc version 7.5.0 (Ubuntu/Linaro 7.5.0-3ubuntu1~18.04)"
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "4.19.36-ge90b30c67"
